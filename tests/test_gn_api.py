f320eb63471f051e6f0a618c1a641b51a45708dc
Jean-Michel Crepel <jean-michel.crepel@camptocamp.com>
2025-01-31 13:46:29 +0100

    takes remarks from pr

--- tests/test_gn_api.py
+++ tests/test_gn_api.py
@@ -87,3 +87,16 @@ def test_upload_zip(init_gn):
         m.post('http://geonetwork/api/records', json=record_callback)
         zipdata = BytesIO(b"dummy_zip")
         init_gn.put_record_zip(zipdata)
+
+
+
+def test_search(init_gn):
+    with requests_mock.Mocker() as m:
+
+        def record_callback(request, context):
+            assert request.headers.get("accept") == "application/json"
+            assert request.headers.get('X-XSRF-TOKEN') == "dummy_xsrf"
+            return {"created": "success"}
+        m.post('http://geonetwork/api/records', json=record_callback)
+        query_to_search = '{"query":{"bool":{"must":[{"terms":{"isTemplate":["n"]}},{"multi_match":{"query":"test","type":"bool_prefix","fields":["resourceTitleObject.*^4","resourceAbstractObject.*^3","tag^2","resourceIdentifier"]}}],"must_not":{"terms":{"resourceType":["service","map","map/static","mapDigital"]}}}},"_source":["resourceTitleObject","uuid"],"from":0,"size":20}'
+        init_gn.search(query_to_search)

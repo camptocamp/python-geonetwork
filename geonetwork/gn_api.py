06413d6436e509cca436a4accc03b07e12818124
Jean-Michel Crepel <jean-michel.crepel@camptocamp.com>
2025-11-03 14:57:14 +0100

    change type of var

--- geonetwork/gn_api.py
+++ geonetwork/gn_api.py
@@ -57,7 +57,7 @@ class GnApi:
         logger.info("GN API Session started with geonetwork server version %s", version)
         return resp
 
-    def get_record_zip(self, uuid: str, withrelated: bool | None = False) -> IO[bytes]:
+    def get_record_zip(self, uuid: str, withrelated: bool = False) -> IO[bytes]:
         """
          retrieve the metadata for `uuid` as a zip archive including linked media.
         :param uuid: uuid of the metadata
